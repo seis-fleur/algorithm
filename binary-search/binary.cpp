3a0cc1647c92df1ea2a152ed98b6380f358d8728
Angeline Basbas <100509078+seis-fleur@users.noreply.github.com>
2022-03-02 18:41:23 +0800

    Update binary.cpp

--- binary-search/binary.cpp
+++ binary-search/binary.cpp
@@ -2,47 +2,64 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <map>
 
 using namespace std;
 
 int main()
 {
     int target = 0, high = 0, mid = 0, low  = 0;
+    map <int, string> Movies;
+    vector<int> years;
     bool found = false;
-    string input_list;
-    vector<int> v = {};
-    string number;
-    ifstream infile;
-    infile.open("binary_test.txt");
+    int year = 0;
+    string title = "";
+    string line;
 
-    getline (infile, input_list);
-    stringstream ss(input_list);
-   
-    while (getline(ss, number, ' '))
-        v.push_back(stoi(number));
-    
-    infile >> target;
+    ifstream infile;
+
+    infile.open("binary_test.in");
+
+    while (getline(infile, line))
+    {
+        year = stoi(line.substr(0, 4));
+        title = (line.substr(5, string::npos));
+        years.push_back(year);
+        Movies.insert(make_pair(year, title));
+    }
+    cout<<"Choose year: "<<endl;
+    for (int year: years)
+    {
+        cout<<year<<"\t";
+    }
+    cout<<endl;
+    cin>>target;
     infile.close();
 
-    high = v.size() - 1;
+    high = Movies.size() - 1;
   
-    while (low <= high)
-    {
-        mid = low + (high - low)/2;
-        if (v[mid] == target)
+     while (low <= high)
+     {
+         mid = low + (high - low)/2;
+         if (years[mid] == target)
+         {
+             found = true;
+             break;
+         }
+         if (years[mid] > target)
+             high = mid - 1;
+         else
+             low = mid + 1;
+     }
+
+     if (found)
         {
-            found = true;
-            break;
+            cout<<"An MCU movie released on "<<target<<" is " <<Movies[target]<<"."<<endl;
+            cout<<"It is found at index "<<mid<<"."<<endl;
         }
-        if (v[mid] > target)
-            high = mid - 1;
-        else
-            low = mid + 1;
-    }
-    if (found)
-        cout<<target<<" is found at index "<<mid<<"."<<endl;
-    else
-        cout<<target<<" is not found."<<endl;
+     else
+         cout<<"There is no MCU movie released on."<<target<<endl;
 
     return 0;
 }
+
